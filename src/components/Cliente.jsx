53cd26bbcea005298dc28b64e39632024c25c13f
drkoop <dr.koop.development@gmail.com>
2023-05-26 21:15:47 -0600

    deploy

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -4,7 +4,7 @@ const Cliente = ({cliente}) => {
     const {nombre, empresa, email, telefono, id} = cliente
 
     return (
-        <tr>
+        <tr className="test">
             <td className="p-6">
                 {nombre}
             </td>
